5aa3a60ee1fa77467edb49fd7f47388b59e00ca0
darlingson <dmakuwila@gmail.com>
2024-02-01 17:50:37 +0200

    initial commit from local to remote

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Malawi_Public_Schools_SIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
